2c47afd41df9006a3add62eec20e095941c49d2e
Vishwajeet Singh Chundawat <104882494+Vishwajeet1809@users.noreply.github.com>
2022-07-23 14:33:08 +0530

    Add files via upload

---
+++ Day 3/Day3.js
@@ -0,0 +1,42 @@
+var Num=100;
+var Str="Vishwajeet Singh Chundawat";
+var BooleanVar=true;
+
+
+document.write("<br/>",Num);
+document.write("<br/>",Str);
+document.write("<br/>",BooleanVar);
+
+
+document.write("<br/>",typeof(BooleanVar));
+
+
+
+var obj = new Object();
+document.write("<br/>"+typeof(obj));
+document.write("<br/>"+obj.FirstName+" "+obj.LastName);
+
+
+var car = {
+    modal: "Dodge SRT Demon",
+    color: "Red",
+    doors: 2
+}
+document.write("<br/>"+car.modal+" "+car.color+" "+car.doors);
+
+
+var cars = ["DEMON", "Mustang", "Skyline"];
+document.write("<br/>"+cars[0]);
+document.write("<br/>"+cars[1]);
+document.write("<br/>"+cars[2]);
+
+
+
+var Demo = function(){ 
+    return "Hello World!"; 
+}
+document.write("<br/>"+typeof(Demo));
+document.write("<br/>"+Demo());
+
+
+
